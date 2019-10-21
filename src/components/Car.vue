01adf3b0dcced528053227dc8cfa019aca57296e
Hanna Tagen <hanna.tagen@gmail.com>
2019-10-22 01:11:55 +0300

    added /car endpoint

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h3>Your Nobe:</h3>
-    <img src="../assets/car.jpg" height="966" width="958"/>
     <h4>{{car}}</h4>
+    <img src="../assets/car.jpg" height="966" width="958"/>
   </div>
 </template>
 
@@ -12,17 +12,15 @@
     export default {
         name: "Car",
         props: {
-            carId: String
+            carId: '',
         },
         data() {
             //TODO import bootstrap for a easier design https://bootstrap-vue.js.org/docs/
             //TODO use cards from bootstrap to display car and data https://bootstrap-vue.js.org/docs/components/card/#cards
 
-            //console.log(this.props.carId);
-            axios.get("/car/" + "nob001") //TODO get carId passed to compoent: "/car/" + this.props.carId
+            axios.get("/car/nob001") //TODO get carId passed to compoent: "/car/" + this.props.carId
                  .then(response => {
                      this.car = response.data; // JSON are parsed automatically.
-                     console.log(this.car);
                  })
                  .catch(e => {
                      console.log(e);
